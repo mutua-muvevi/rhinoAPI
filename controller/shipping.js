8a9c800035790a76ddf3f4342b4d69e9f138b85a
SamsAgency <samstore637@gmail.com>
2022-03-29 10:32:37 +0300

    added delete and update routes for storage and shipment

--- controller/shipping.js
+++ controller/shipping.js
@@ -3,10 +3,16 @@ const ErrorResponse = require("../utils/errorResponse");
 
 // post
 exports.postShipment = async (req, res, next) => {
-	// const { trackno, shippersfullname, departurecity, departurecountry, departuredate, consignfullnames, collectorfullname, collectoraddress, collectortel, itemsname, departuretime, arrivalcity, arrivalcountry, arrivaltime, arrivaldate, shippersemail, shippersidno, shipperstelephone, shipperscompany, shippersaddress, consigntelephone, consignemail, consigncompany, consignaddress, logisticstype, itemsweight, itemsweightunit, itemsproducttype, itemspieces, itemsquality, quantifiableunit, events } = req.body
+	const { trackno, shippersfullname, departurecity, departurecountry, departuredate, consignfullnames, collectorfullname, collectoraddress, collectortel, itemsname, departuretime, arrivalcity, arrivalcountry, arrivaltime, arrivaldate, shippersemail, shippersidno, shipperstelephone, shipperscompany, shippersaddress, consigntelephone, consignemail, consigncompany, consignaddress, logisticstype, itemsweight, itemsweightunit, itemsproducttype, itemspieces, itemsquality, quantifiableunit, events } = req.body
 
 	try {
-		const shipping = await Shipping.create(req.body)
+		const existing = await Shipping.findOne({trackno})
+
+		if(existing){
+			return next(new ErrorResponse("That track number already exists", 400))
+		}
+
+		const shipping = await Shipping.create({ trackno, shippersfullname, departurecity, departurecountry, departuredate, consignfullnames, collectorfullname, collectoraddress, collectortel, itemsname, departuretime, arrivalcity, arrivalcountry, arrivaltime, arrivaldate, shippersemail, shippersidno, shipperstelephone, shipperscompany, shippersaddress, consigntelephone, consignemail, consigncompany, consignaddress, logisticstype, itemsweight, itemsweightunit, itemsproducttype, itemspieces, itemsquality, quantifiableunit, events })
 
 		res.status(201).json({
 			success: true,
@@ -18,6 +24,30 @@ exports.postShipment = async (req, res, next) => {
 
 }
 
+// update entire shipment
+exports.updateEntireShipping = async (req, res, next) => {
+	const { trackno, shippersfullname, departurecity, departurecountry, departuredate, consignfullnames, collectorfullname, collectoraddress, collectortel, itemsname, departuretime, arrivalcity, arrivalcountry, arrivaltime, arrivaldate, shippersemail, shippersidno, shipperstelephone, shipperscompany, shippersaddress, consigntelephone, consignemail, consigncompany, consignaddress, logisticstype, itemsweight, itemsweightunit, itemsproducttype, itemspieces, itemsquality, quantifiableunit, events } = req.body
+
+	try {
+		const shipping = await Shipping.findOneAndUpdate(
+			{ trackno },
+			{ trackno, shippersfullname, departurecity, departurecountry, departuredate, consignfullnames, collectorfullname, collectoraddress, collectortel, itemsname, departuretime, arrivalcity, arrivalcountry, arrivaltime, arrivaldate, shippersemail, shippersidno, shipperstelephone, shipperscompany, shippersaddress, consigntelephone, consignemail, consigncompany, consignaddress, logisticstype, itemsweight, itemsweightunit, itemsproducttype, itemspieces, itemsquality, quantifiableunit, events },
+			{ new: true }
+		)
+
+		if(!shipping) {
+			return next(new ErrorResponse("That shipping record does not exist", 404))
+		}
+
+		res.status(200).json({
+			success: true,
+			data: shipping
+		})
+	} catch (error) {
+		next(error)
+	}
+}
+
 // update an event
 exports.updateShipping = async (req, res, next) => {
 	try {
@@ -27,7 +57,7 @@ exports.updateShipping = async (req, res, next) => {
 
 		const shipping = await Shipping.findOne({trackno})
 
-		// console.log("The event array is", shipping.events)
+
 		shipping.events.push({ timeevents, dateevents, currentlocation, shippingstatus, notes, number})
 		
 		await shipping.save()
@@ -70,6 +100,27 @@ exports.getShippingById = async (req, res, next) => {
 		res.status(200).json({
 			success: true,
 			data: shipping
+		})
+
+	} catch (error) {
+		next(error)
+	}
+}
+
+// delete a given shipment
+exports.deleteShipping = async (req, res, next) => {
+
+
+	try {
+		const shipping = await Shipping.findByIdAndDelete(req.params.id)
+
+		if(!shipping){
+			return next(new ErrorResponse("That shipping record does not exist", 404))
+		}
+
+		res.status(200).json({
+			success: true,
+			data: "The shipping item was deleted successfully"
 		})
 
 	} catch (error) {
--- routes/shipping.js
+++ routes/shipping.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { postShipment, getAllShippingRecords, getShippingById, updateShipping } = require("../controller/shipping")
+const { postShipment, getAllShippingRecords, getShippingById, updateShipping, updateEntireShipping, deleteShipping } = require("../controller/shipping")
 const { onlyLoggedIn, onlyAdmin } = require("../middleware/auth")
 
 router.route("/post").post(onlyAdmin, postShipment);
 router.route("/item/:id").get(onlyLoggedIn, getShippingById)
-router.route("/item/update").put(onlyAdmin, updateShipping)
+router.route("/event/update").put(onlyAdmin, updateShipping)
+router.route("/item/update").put(onlyAdmin, updateEntireShipping)
 router.route("/all").get(onlyAdmin, getAllShippingRecords);
-
+router.route("/delete/:id").delete(onlyAdmin, deleteShipping)
 
 module.exports = router
