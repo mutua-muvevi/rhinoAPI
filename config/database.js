328252ca72ade0296b8201f8b133195761d6dd21
SamsAgency <samstore637@gmail.com>
2022-04-05 13:13:49 +0300

    removed the test throw error

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const logger = require("../utils/logger");
 
 const connectDB = async () => {
-	throw new Error("Test : Something teribly went wrong")
+	// throw new Error("Test : Something teribly went wrong")
 	try {
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
