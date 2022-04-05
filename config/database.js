a2bde18bd8e93109b9e727f4780c20b0f87045f2
SamsAgency <samstore637@gmail.com>
2022-04-05 13:07:31 +0300

    worked on console and file logger with winston

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
 
 const connectDB = async () => {
+	throw new Error("Test : Something teribly went wrong")
 	try {
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
 
-		console.log("Connected to the Database")
+		logger.info("Connected to the Database")
 		
-	} catch (error) {
-		console.log(`Database Connection Error : ${error}`)
+	} catch (err) {
+		logger.error(`Database Connection Error : ${err}`)
 	}
 }
 
