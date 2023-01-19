a96fdabc888185b2a261e1e13af09e617d354cbc
mutua-muvevi <samstore637@gmail.com>
2023-01-19 11:29:16 +0300

    edit loggger

--- utils/logger.js
+++ utils/logger.js
@@ -12,14 +12,14 @@ module.exports = createLogger({
 			)
 		}),
 
-		new transports.File({
-		filename: 'logs/server.log',
-		level: 'info',
-		format:format.combine(
-			format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-			format.align(),
-			format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-		)}),
+		// new transports.File({
+		// filename: 'logs/server.log',
+		// level: 'info',
+		// format:format.combine(
+		// 	format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+		// 	format.align(),
+		// 	format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+		// )}),
 
 		
 		new transports.MongoDB({
